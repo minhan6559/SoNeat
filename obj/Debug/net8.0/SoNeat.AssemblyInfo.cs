e393c22ca611d2036eb9c88d62615bedf44bd9d8
minhan6559 <minhan6559@gmail.com>
2024-08-26 21:39:30 +1000

    update gitignore

--- obj/Debug/net8.0/SoNeat.AssemblyInfo.cs
+++ obj/Debug/net8.0/SoNeat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoNeat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba70bc90fa9834a84365b98f9b3c0c7ac79dd989")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2317ce8540ce32f23e7c8bebf963c6d2cfdb5ea3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoNeat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoNeat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
